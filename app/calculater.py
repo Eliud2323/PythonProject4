b879328bc6ff50ae94c8c4d70b6f664a88b45da1
Mbappe <jpablohh4@gmail.com>
2025-02-06 11:37:00 -0500

    implementé el metodo multiplicar

--- app/calculater.py
+++ app/calculater.py
@@ -2,5 +2,8 @@ class Calculator:
     def add(self, a , b):
         return a + b
 
-    def add(self, a , b):
+    def subtract(self, a , b):
         return a - b
+
+    def multiply(self, a , b):
+        return a * b
